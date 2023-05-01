3305f213641ab71d669d97f645cc8621f869672e
Tom Moyse <thomashmoyse@gmail.com>
2023-05-01 18:29:49 +0800

    Implemented ability to calcuate phrase docposting at query time

--- Search.java
+++ Search.java
@@ -9,14 +9,6 @@ public class Search {
 
     Search() throws IOException{
         info = new InfoStore();
-        test();
-    }
-
-    private void test() throws IOException{
-        // 'movi' id is 12
-        Integer id = 12;
-        DocPostings dp = info.getKeywordPostingBody(id);
-        System.out.println(dp.getDocFreq());
     }
 
     public void EvaluateQuery(String q) throws IOException{
@@ -87,5 +79,91 @@ public class Search {
             if (f > tfmax){ tfmax = f; }
         }
         return tf * idf / tfmax;
+    }
+
+    // Phrase assumed to only contain valid stemmed keywords
+    private DocPostings computeBigramDP(String[] phrase) throws IOException{
+        // Find phrase locations
+        DocPostings w1 = info.getKeywordPostingBody(info.getKeywordID(phrase[0]));
+        DocPostings w2 = info.getKeywordPostingBody(info.getKeywordID(phrase[1]));
+
+        Integer[] w1Docs = w1.getDocumentIDs();
+
+        ArrayList<Integer> w1Positions;
+        ArrayList<Integer> w2Positions;
+
+        DocPostings newDP = new DocPostings();
+
+        // Populate new doc posting with phrase
+        for (Integer docID : w1Docs) {
+            if ((w2Positions = w2.getPositionList(docID)) == null){ continue; }
+            w1Positions = w1.getPositionList(docID);
+
+            // Check for consecutive terms
+            int w1ind = 0;
+            int w2ind = 0;
+            int t1;
+            int t2;
+            while (w1ind < w1Positions.size() && w2ind < w2Positions.size()){
+                t1 = w1Positions.get(w1ind).intValue();
+                t2 = w2Positions.get(w2ind).intValue();
+                if (t1 + 1 == t2){
+                    newDP.addPosting(docID, 0);
+                    t1++;
+                    t2++;
+                }
+                else if (t1 < t2) { t1++; }
+                else { t2++; }
+            }
+        }
+
+        return newDP;
+    }
+
+    // Phrase assumed to only contain valid stemmed keywords
+    private DocPostings computeTrigramDP(String[] phrase) throws IOException{
+        // Find phrase locations
+        DocPostings w1 = info.getKeywordPostingBody(info.getKeywordID(phrase[0]));
+        DocPostings w2 = info.getKeywordPostingBody(info.getKeywordID(phrase[1]));
+        DocPostings w3 = info.getKeywordPostingBody(info.getKeywordID(phrase[2]));
+
+        Integer[] w1Docs = w1.getDocumentIDs();
+
+        ArrayList<Integer> w1Positions;
+        ArrayList<Integer> w2Positions;
+        ArrayList<Integer> w3Positions;
+
+        DocPostings newDP = new DocPostings();
+
+        // Populate new doc posting with phrase
+        for (Integer docID : w1Docs) {
+            if ((w2Positions = w2.getPositionList(docID)) == null){ continue; }
+            if ((w3Positions = w3.getPositionList(docID)) == null){ continue; }
+            w1Positions = w1.getPositionList(docID);
+
+            // Check for consecutive terms
+            int w1ind = 0;
+            int w2ind = 0;
+            int w3ind = 0;
+            int t1;
+            int t2;
+            int t3;
+            while (w1ind < w1Positions.size() && w2ind < w2Positions.size() && w3ind < w3Positions.size()){
+                t1 = w1Positions.get(w1ind).intValue();
+                t2 = w2Positions.get(w2ind).intValue();
+                t3 = w3Positions.get(w3ind).intValue();
+                if (t1 + 1 == t2 && t2 + 1 == t3){
+                    newDP.addPosting(docID, 0);
+                    t1++;
+                    t2++;
+                    t3++;
+                }
+                else if (t1 <= t2 && t1 <= t3) { t1++; }
+                else if (t2 <= t1 && t2 <= t3) { t2++; }
+                else { t3++; }
+            }
+        }
+
+        return newDP;
     }
 }
